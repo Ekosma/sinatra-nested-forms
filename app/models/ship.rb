1805637aa619b60b5f1886968f6fa85f1b407300
Evelyn <evelyn.a.kosma@gmail.com>
2021-08-19 21:40:32 -0500

    done

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+    
+    attr_reader :name, :type, :booty_attributes
+
+    SHIPS = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty_attributes = params[:booty_attributes]
+        PIRATES << self
+      end
+
+    def self.all 
+        SHIPS
+    end
+    
+    def self.clear
+        SHIPS.clear
+    end
+
 end
